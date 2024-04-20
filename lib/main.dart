2aee97ed8ee3fa409d3bfe5c1e2e3972c387d6ef
moha_kh_23 <khrijimohamedahmed@gmail.com>
2024-04-20 22:24:38 +0200

    therd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'quiz_brain.dart';
+import 'package:quiiz/dachboord/home.dart';
+import 'lol/quiz_brain.dart';
 
 QuizBrain quizBrain = QuizBrain();
 
@@ -15,111 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: Moha(),
-      ),
-    );
-  }
-}
-
-class Moha extends StatefulWidget {
-  const Moha({Key? key});
-
-  @override
-  State<Moha> createState() => _MohaState();
-}
-
-class _MohaState extends State<Moha> {
-  List<Icon> scorekeeper = [];
-
-  void checkAnswer(bool userPickAnswer) {
-    bool correctAnswer = quizBrain.getCorrectAnser();
-    setState(() {
-      if (userPickAnswer == correctAnswer) {
-        scorekeeper.add(Icon(
-          Icons.check,
-          color: Colors.green,
-        ));
-      } else {
-        scorekeeper.add(Icon(
-          Icons.close,
-          color: Colors.red,
-        ));
-      }
-      quizBrain.nextQuestion();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            flex: 1,
-            child: Image.asset('assets/backgroundd.png'),
-          ),
-          Expanded(
-            flex: 5,
-            child: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Center(
-                child: Text(
-                  quizBrain.getQuestionText(),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 25.0, color: Colors.white),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Container(
-                color: Colors.green,
-                height: 50.0,
-                width: 50.0,
-                child: TextButton(
-                  onPressed: () {
-                    //the user picked true,
-                    checkAnswer(true);
-                  },
-                  child: Text(
-                    'True',
-                    style: TextStyle(color: Colors.white, fontSize: 25.0),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Container(
-                color: Colors.red,
-                height: 50.0,
-                width: 50.0,
-                child: TextButton(
-                  onPressed: () {
-                    //the user picked false,
-                    checkAnswer(false);
-                  },
-                  child: Text(
-                    'False',
-                    style: TextStyle(color: Colors.white, fontSize: 25.0),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: scorekeeper,
-            ),
-          ),
-        ],
+        body: MyHomePage(),
       ),
     );
   }
